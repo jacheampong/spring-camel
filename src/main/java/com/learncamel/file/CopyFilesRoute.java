0f990f4440b75cd7aefce0da623221ba3366cc1d
jacheampong <jacheampong@gmail.com>
2018-12-18 11:03:39 -0500

    Test Camel Route #2

---
+++ src/main/java/com/learncamel/file/CopyFilesRoute.java
@@ -0,0 +1,15 @@
+package com.learncamel.file;
+
+import org.apache.camel.builder.RouteBuilder;
+
+
+public class CopyFilesRoute extends RouteBuilder {
+
+    public void configure() throws Exception {
+        from("file:data/input?noop=true")
+                // use showHeaders  to log more info on data transfer
+                .to("log:?level=INFO&showBody=true&showHeaders=true")
+                .to("file:data/output");
+    }
+
+}
